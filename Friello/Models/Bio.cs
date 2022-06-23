c0eb6e231406c5fdb2e4da9416adaacd1ad87960
Samir Z. Amiraslanov <samirza@code.local>
2022-06-23 06:41:06 -0700

    feat:format

---
+++ Friello/Models/Bio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Friello.Models
+{
+    public class Bio
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string AuthorName { get; set; }
+        public string Facebook { get; set; }
+        public string Linkedin { get; set; }
+    }
+}
---
+++ Friello/ViewComponents/ProductViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Friello.DAL;
+using Friello.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Friello.ViewComponents
+{
+    public class ProductViewComponent:ViewComponent
+    {
+        private readonly AppDbContext _context;
+        public ProductViewComponent (AppDbContext context)
+        {
+            _context = context; 
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int take)
+        {
+            List<Product> products = _context.Products.Include(p => p.CategoryName).Take(take).ToList();
+            return View(await Task.FromResult(products));
+        }
+    }
+}
